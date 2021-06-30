8a16f52809a2093c416a88b3655f79d35533629e
group120hcmus <group120@student.hcmus.edu.vn>
2021-06-30 09:56:05 +0700

    Add view template

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const createError = require('http-errors');
 const express = require('express');
+const expressLayouts = require('express-ejs-layouts');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
@@ -13,12 +14,15 @@ config.connectDB().then();
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
+app.use(expressLayouts);
+app.set('layout', './layouts/full-layout');
+app.set('layout extractScripts', true); // for specific page script
 
-app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use('/assets/', express.static(path.join(__dirname, 'public')));
+app.use(logger('dev'));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
